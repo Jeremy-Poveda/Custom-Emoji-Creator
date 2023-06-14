35fbd9a9269c99cef82b3c0aec313692be995322
JeryRPG <jerypovedag@gmail.com>
2023-06-13 21:42:46 -0500

    Creación de la carpeta modelo, creación interfaz List y clase DNode y esqueleto de la estructura de lista circular doblemente enlazada.

---
+++ customEmojis/src/main/java/espol/grupo4/customemojis/Model/CircularDoubleLinkedList.java
@@ -0,0 +1,15 @@
+package espol.grupo4.customemojis.Model;
+
+public class CircularDoubleLinkedList<T> implements List{
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void addLast(Object element) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
---
+++ customEmojis/src/main/java/espol/grupo4/customemojis/Model/List.java
@@ -0,0 +1,6 @@
+package espol.grupo4.customemojis.Model;
+
+public interface List<T> {
+    public int size();
+    public void addLast(T element);
+}
